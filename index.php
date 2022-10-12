b114b1457030ba95869d675a09cca8fafa6e54ed
Ventur van Rooijen <99063409@mydavinci.nl>
2022-10-12 13:49:09 +0200

    Starting Page

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To Do List</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class='container'>
+
+        <div class="list">
+                <div class="listName">
+                    <h1>listName</h1>
+                </div>
+                <div class="task">
+                    <h1>taskName</h1>
+                <p>task</p>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
